66a9ab246b5d9e08b5c607203e3724041ca0e71c
tianyuningmou <tianyuningmou2016@126.com>
2017-12-25 18:38:38 +0800

    connect_neo4j

---
+++ connect_neo4j.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2017
+
+All rights reserved by easyto
+
+FILE:  connect_neo4j.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2017/12/25 下午5:51
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2017/12/25 下午5:51
+"""
+
+
