de5aacefb85fc7e2e7309e24b687e4df8b32b04a
tianyuningmou <tianyuningmou2009@126.com>
2017-12-26 17:40:05 +0800

    test

---
+++ test_neo4j.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2017
+
+All rights reserved by easyto
+
+FILE:  test_neo4j.py
+AUTHOR:  tianyuningmou
+DATE CREATED:  @Time : 2017/12/25 下午5:51
+
+DESCRIPTION:  .
+
+VERSION: : #1 $
+CHANGED By: : tianyuningmou $
+CHANGE:  :  $
+MODIFIED: : @Time : 2017/12/25 下午5:51
+"""
+
+from neo4j.v1 import GraphDatabase
+
+class HelloWorldExample(object):
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def print_greeting(self, message):
+        with self._driver.session() as session:
+            greeting = session.write_transcation(self._create_and_return_greeting, message)
+            print(greeting)
+
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a: GREETING) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
--- test_py2neo.py
+++ test_py2neo.py
@@ -5,15 +5,16 @@ Copyright () 2017
 
 All rights reserved by easyto
 
-FILE:  connect_neo4j.py
+FILE:  test_py2neo.py
 AUTHOR:  tianyuningmou
-DATE CREATED:  @Time : 2017/12/25 下午5:51
+DATE CREATED:  @Time : 2017/12/26 下午2:24
 
 DESCRIPTION:  .
 
 VERSION: : #1 $
 CHANGED By: : tianyuningmou $
 CHANGE:  :  $
-MODIFIED: : @Time : 2017/12/25 下午5:51
+MODIFIED: : @Time : 2017/12/26 下午2:24
 """
+
 
